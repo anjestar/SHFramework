9d0e869c84d7baa963de62d3cab9aa9e5371a640
duanyong <coderduan@gmail.com>
2012-04-20 19:10:39 +0800

    devinc.common.php

--- v.01/devinc.common.php
+++ v.01/devinc.common.php
@@ -1,8 +1,10 @@
 <?php
 
-define("FRAMEWORK_DIR", dirname(__DIR__));
+define("FRAMEWORK_DIR", dirname(__FILE__));
 
 
 require_once(FRAMEWORK_DIR . "/devinc.bad.php");
 require_once(FRAMEWORK_DIR . "/devinc.sql.php");
 require_once(FRAMEWORK_DIR . "/devinc.mdb2.php");
+
+require_once(FRAMEWORK_DIR . "/devinc.action.php");
