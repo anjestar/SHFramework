c1cf1b8df031ce6f867ac4af47d8e01f66a20090
duanyong <coderduan@gmail.com>
2012-04-21 19:17:02 +0800

    devinc.user.php

--- v.01/devinc.user.php
+++ v.01/devinc.user.php
@@ -4,9 +4,8 @@
 // devinc.user.php
 //	用户操作的函数
 //
-//	s_user_logined()
-//	    返回用户是否登录的状态
-//  
+//  s_user_by_uid($uid)
+//      根据uid返回用户的基本信息
 //
 //
 //
@@ -19,42 +18,21 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-//检查用户是否登录
-function s_user_is_logined() {
-    return s_sso_auth() !== false;
-}
-
-
 //获取用户的信息（先从缓存中获取，再从API中获取）
-function s_user_data_by_uid($uid=false) {
-    if (s_bad_id($uid)
-        || ( $mem = s_memcache_global() ) === false
-    ) {
+function s_user_by_uid($uid) {
+    if (s_bad_id($uid)) {
         return false;
     }
 
-	$key = md5(MEM_CACHE_KEY_PREFIX . "_user_" . $uid);
+	$key = "user_by_uid#" . $uid);
+    $arr = array(
+        "uid"   => $uid,
+    );
 
-    if (!( $user = $mem->get($key) )
-        || !( $user = json_decode($user, true) )
+    if (false === ( $user = s_memcache_get($key) )
+        || false === ( $user = s_weibo_http("", $arr) )
     ) {
-        $weibo  = new WeiboClient(MBLOG_APP_KEY, MBLOG_APP_SKEY, OAUTH_USER, OAUTH_PASS);
-        $info   = $weibo->show_user($uid);
-
-        if($info['id']) {
-            $user['id']                 = $info['id'];
-            $user['attnum']             = $info['followers_count'] ? $info['followers_count']: 0; //粉丝数
-            $user['domain']             = $info['domain'] ? $info['domain'] : "";
-            $user['location']           = $info['location'] ? $info['location'] : "";
-            $user['attention']          = $info['friends_count'] ? $info['friends_count'] : 0; //关注数
-            $user['mblogCount']         = $info['statuses_count'] ? $info['statuses_count'] : 0; //发微博数
-            $user['screen_name']        = $info['screen_name'] ? $info['screen_name'] : "";
-            $user['profile_image_url']  = $info['profile_image_url'] ? $info['profile_image_url'] : "";
-            $user['favourites_count']   = $info['favourites_count'] ? $info['favourites_count'] : 0; //收藏数
-
-            //缓存1小时
-            $mem->set($key, json_encode($user), 0, 3600);
-        }
+        return false;
     }
 
 	return $user;
@@ -64,59 +42,165 @@ function s_user_data_by_uid($uid=false) {
 
 //按screen_name获取用户信息
 function s_user_by_nickname($nickname) {
-    if (s_bad_string($nickname)
-        || ( $mem = s_memcache_global() ) === false
-    ) {
+    if (s_bad_string($nickname)) {
         return false;
     }
 
-    $key = md5(MEM_CACHE_KEY_PREFIX . "_nickname_" . $nickname);
+    $key = "user_by_nickname#" . $nickname
 
-    if (false === ( $uid = $mem->get($key) )) {
+    if (false === ( $uid = s_memcache_get($key) )) {
         //缓存中不存在，从API获取uid缓存起来
-        if ( false === ( $user = s_http_request("/users/show.json", array("screen_name" => $nickname)) )
-            || s_bad_id($user["id"], $uid)
-        ) {
-            return false;
-        }
+        $params = array(
+            "screen_name"   => $nickname,
+        );
 
         //缓存1小时
-        $mem->set($key, $uid, 0, 3600);
+        //$mem->set($key, $uid, 0, 3600);
     }
 
     return s_user_data_by_uid($uid);
 }
 
 
-//通过个性域名获取用户信息
-//$domain: 只需要为hiduan即可，不需要http://weibo.com/hiduan
+//通过个性域名获取用户信息（http://weibo.com/hiduan: $domain => "hiduan"）
 function s_user_by_domain($domain) {
-    if (s_bad_string($domain)
-        || ( $mem = s_memcache_global() ) === false
-    ) {
+    if (s_bad_string($domain, $domain)) {
         return false;
     }
 
-    $key = md5(MEM_CACHE_KEY_PREFIX . "_domain_" . $domain);
+    $key    = "user_by_domain_" . $domain;
+    $params = array("domain" => $domain);
 
-    if (false === ( $uid = $mem->get($key) )) {
-        //缓存中不存在，从API获取uid缓存起来
-        if ( false === ( $user = s_http_request("/users/domain_show.json", array("domain" => $domain)) )
-            || s_bad_id($user["id"], $uid)
-        ) {
-            return false;
-        }
-
-        //缓存1小时
-        $mem->set($key, $uid, 0, 3600);
+    if (false === ( $data = s_memcache_get($key) )
+        false === ( $user = s_weibo_http("/users/domain_show.json", $params) )
+    ) {
+        //缓存中不存在
+        return false;
     }
 
+
+    //获取uid，从缓存中获取用户信息
     return s_user_by_uid($uid);
 }
 
 
-//获取用户的微博列表，超过200条返回false
-function s_weibo_list_by_uid($uid, $page=1, $count=20) {
+//批量获取用户信息
+function s_users_by_uids(&$uids, $encoded=false) {
+    if (!s_bad_array($uids)
+        || !( $uids = array_unique($uids) )
+        || !( $uids = array_values($uids) )
+        || empty($uids)
+    ) {
+        return false;
+    }
+
+    //看cache中是否存在
+    asort($uids);
+	$mem  = mem_cache_share();
+    $key  = md5(MEM_CACHE_KEY_PREFIX."_user_by_uids_" . implode(",", $uids) . strval($encoded));
+
+    if (( $data = $mem->get($key) )) {
+        //缓存中已经存在
+		$data = json_decode($data, true);
+    }
+
+    if (!$data) {
+        //缓存中没有，请求服务器
+        $max    = 20;
+        $time   = 0;
+        $times  = ceil(count($uids) / $max);
+        $list   = array();
+
+        do {
+            $ids = array();
+
+            $num0 = $time * $max;
+            $num1 = ($time + 1) * $max - 1;
+
+            foreach (range($num0, $num1) as $index) {
+                if (!isset($uids[$index])
+                    || intval($uids[$index]) <= 0
+                ) {
+                    break;
+                }
+
+                $ids[] = $uids[$index];
+            }
+
+            $params = array(
+                "uids"   = implode(",", $ids),
+                "source" = APP_KEY,
+                "cookie" = array(
+                    "SUE"   => $_COOKIE["SUE"],
+                    "SUP"   => $_COOKIE["SUP"],
+                ),
+            );
+
+            $data = s_http_get()
+
+            $req =& new HTTP_Request('http://i2.api.weibo.com/2/users/show_batch.json');    
+            $req->setMethod(HTTP_REQUEST_METHOD_GET);	
+            $req->addCookie("SUE",URLEncode($_COOKIE["SUE"]));
+            $req->addCookie("SUP",URLEncode($_COOKIE["SUP"]));
+            $req->addQueryString('uids', implode(",", $ids));
+            $req->addQueryString('is_encoded', $encoded === false ? 0 : 1);
+            $req->addQueryString('source', MBLOG_APP_KEY);
+
+            $rs = $req->sendRequest();
+
+
+            if (PEAR::isError($rs)
+                || !( $ret = json_decode($req->getResponseBody(), true) )
+                || isset($ret["error_code"])
+            ) {
+                return false;
+            }
+
+            //echo "list:", var_dump($list);
+            //有可能是空数组
+            if (isset($ret["users"])) {
+                $list = array_merge($list, $ret["users"]);
+            }
+
+            unset($ret);
+        } while (( ++ $time ) < $times);
+
+
+        $data = array();
+
+        //重新组合成uid => array()
+        foreach ($list as &$item) {
+            if (isset($item["id"])
+                && $item["idstr"] > 0
+            ) {
+                $data[$item["idstr"]] = $item;
+            }
+
+            unset($item);
+        }
+
+
+        //检查自己是否在数组中
+        if (false !== ( $me = login_user_info() )
+            && ( $meid = $me["uniqueid"] )
+            && in_array($meid, $uids)
+            && ( $me = get_user_by_token(intval($meid)) )
+        ) {
+            $data[$me["id"]] = $me;
+        }
+
+        unset($list);
+       
+        //缓存十小时
+		$mem->set($key, json_encode($data), 0, MEM_CACHE_LIFETIME_LUCKY);
+    }
+
+    return $data;
+}
+
+
+//用户发布徽博
+function s_user_post_weibo($uid, $weibo) {
     if (s_bad_id($uid)
         || ( $mem = s_memcache_global() ) === false
     ) {
