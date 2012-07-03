8538527ed5b744600234d258b4d9fcbfef971bb3
duanyong <xuyong3@staff.sina.com.cn>
2012-07-03 15:27:31 +0800

    v.06版本

---
+++ v.06/devinc.bad.php
@@ -0,0 +1,285 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//
+// devinc.bad.php
+//	判断错误的函数，参数错误返回true，正确返回false
+//
+//  s_bad_id($id)
+//	    判断数字是否正确（大于0）
+//  
+//  s_bad_0id($id)
+//	    判断数字是否正确（等于0也可以）
+//  
+//  s_bad_string($string)
+//	    判断字符串是否正确
+//
+//  s_bad_array($string, &$var)
+//	    判断数组否是正确，如果正确赋值给$var变量
+//
+//  s_bad_email($email, $var)
+//	    判断邮箱地址是否正确
+//
+//  s_bad_post($key, $var=false, $type="string")
+//      判断POST中的$key是否正确
+//          $type为: string, int, int0, array, email, phone(包含mobile和telphone), mobile, telphone, image
+//
+//  s_bad_get($key, $var=false, $method="string")
+//    判断GET中的$key是否正确
+//          $type为: string, int, int0, array, email, phone(包含mobile和telphone), mobile, telphone, image
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+function s_bad_id(&$id, &$var=false) {
+    if(!is_numeric($id)
+        || ( $id = intval($id) ) <= 0
+    ) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $id;
+    }
+
+    return false;
+}
+
+
+function s_bad_0id(&$id, &$var=false) {
+    if(!is_numeric($id)
+        || ( $id = intval($id) ) < 0
+    ) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $id;
+    }
+
+    return false;
+}
+
+
+function s_bad_string(&$str, &$var=false, $trim=true) {
+    if (!is_string($str)
+        || $str !== strval($str)
+        || trim($str) === ""
+    ) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = strval($str);
+    }
+
+    if ($trim) {
+        $var = trim($var);
+    }
+
+    return false;
+}
+
+
+function s_bad_0string(&$str, &$var=false) {
+    if (!is_string($str)
+        || $str !== strval($str)
+    ) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $str;
+    }
+
+    return false;
+}
+
+function s_bad_array(&$arr, &$var=false) {
+    if (!is_array($arr)
+        || empty($arr)
+    ) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $arr;
+    }
+
+    return false;
+}
+
+
+function s_bad_email(&$email, &$var=false) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $email;
+    }
+
+    return false;
+}
+
+
+function s_bad_mobile($mobile, &$var=false) {
+    if (!preg_match("/^1(3|4|5|6|7|8|9)\d{9}$/", $mobile)) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $mobile;
+    }
+
+    return false;
+}
+
+
+function s_bad_telphone($phone, &$var=false) {
+    if (s_bad_string($phone)
+        || !preg_match("/^(\d+\-)*\d+$/", $phone)
+    ) {
+        return true;
+    }
+
+    if ($var !== false) {
+        $var = $phone;
+    }
+
+    return false;
+}
+
+
+//返回post值
+function s_bad_post($key, &$var=false, $type="string", $html=true) {
+    if (s_bad_string($key)
+        || !isset($_POST[$key])
+    ) {
+        return true;
+    }
+
+
+    if ($type === "string") {
+        //字符类型
+        if ($html !== true) {
+            //不需要转义，直接返回判断结果
+            return s_bad_string($_POST[$key], $var);
+        }
+
+        //需要对参数转义处理
+        if (true === s_bad_string($_POST[$key], $var)) {
+            //不需要转义，因为参数已经验证失败
+            return true;
+        }
+
+        if ($var !== false) {
+            $var = s_safe_html($var);
+        }
+
+        //验证成功，此处返回
+        return false;
+
+    } else if ($type === "int") {
+        //整型
+        return s_bad_id($_POST[$key], $var);
+
+    } else if ($type === "int0") {
+        //整型，可以为0
+        return s_bad_0id($_POST[$key], $var);
+
+    } else if ($type === "array") {
+        //数组
+        return s_bad_array($_POST[$key], $var);
+
+    } else if ($type === "email") {
+        //邮箱
+        return s_bad_email($_POST[$key], $var);
+
+    } else if ($type === "phone"
+        || $type === "telphone"
+    ) {
+        //手机或电话（只需要验证telphone，因为telphone的规则很松已经包含手机了）
+        return s_bad_telphone($_POST[$key], $var);
+
+    } else if ($type === "mobile") {
+        //手机
+        return s_bad_mobile($_POST[$key], $var);
+
+    } else if ($type === "image") {
+        //图片（只取request.data中的数据）
+        if (!isset($GLOBALS["HTTP_RAW_POST_DATA"])
+            || !($GLOBALS["HTTP_RAW_POST_DATA"])
+        ) {
+            return true;
+        }
+
+        if ($var !== false) {
+            $var = $GLOBALS["HTTP_RAW_POST_DATA"];
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+
+//返回get值
+function s_bad_get($key, &$var=false, $type="string", $html=true) {
+    if (s_bad_string($key)
+        || !isset($_GET[$key])
+    ) {
+        return true;
+    }
+
+
+    if ($type === "string") {
+        //字符类型
+        if ($html !== true) {
+            //不需要转义，直接返回判断结果
+            return s_bad_string($_GET[$key], $var);
+        }
+
+        //需要对参数转义处理
+        if (true === s_bad_string($_GET[$key], $var)) {
+            //不需要转义，因为参数已经验证失败
+            return true;
+        }
+
+        if ($var !== false) {
+            $var = s_safe_html($var);
+        }
+
+        //验证成功，此处返回
+        return false;
+
+    } else if ($type === "int") {
+        //整型
+        return s_bad_id($_GET[$key], $var);
+
+    } else if ($type === "int0") {
+        return s_bad_0id($_GET[$key], $var);
+
+    } else if ($type === "email") {
+        //邮箱
+        return s_bad_email($_GET[$key], $var);
+
+    } else if ($type === "phone"
+        || $type === "telphone"
+    ) {
+        //手机或电话（只需要验证telphone，因为telphone的规则很松已经包含手机了）
+        return s_bad_telphone($_GET[$key], $var);
+
+    } else if ($type === "mobile") {
+        //手机
+        return s_bad_mobile($_GET[$key], $var);
+    }
+
+    return true;
+}
+
+
+function s_bad_gd() {
+    return extension_loaded('gd') === false;
+}
---
+++ v.06/devinc.weibo.php
@@ -0,0 +1,342 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//
+// devinc.bad.php
+//	判断错误的函数，参数错误返回true，正确返回false
+//
+//  s_weibo_list_by($wid)
+//	    判断数字是否正确（大于0）
+//  
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+function s_weibo_by_wid($wid) {
+    if (s_bad_id($wid)) {
+        return s_err_arg();
+    }
+
+
+    $key = 'weibo_by_wid#' . $wid;
+
+    if (false === ( $data = s_memcache($key) )) {
+        if (false === ( $data = s_weibo_http('https://api.weibo.com/2/statuses/show.json', array('id'=>$wid)) )) {
+            //有些微博会被删除，所以不提示
+            return false;
+        }
+
+        //缓存24小时
+        s_memcache($key, $data, 24 * 3600);
+    }
+
+    return $data;
+}
+
+
+function s_weibo_list_ago($list) {
+    if (s_bad_array($list)) {
+        return false;
+    }
+
+    foreach ($list as &$item) {
+        if (isset($item['time'])) {
+            $item['ago'] = s_weibo_ago($item['time']);
+        }
+
+        unset($item['fdate']);
+        unset($item['ftime']);
+        unset($item['status']);
+
+        unset($item);
+    }
+
+    return $list;
+}
+
+
+function s_weibo_list_time($list, $format="m月d日 H:i", $postfix="") {
+    if (s_bad_array($list)
+        || s_bad_string($format)
+    ) {
+        return false;
+    }
+
+    foreach ($list as &$item) {
+        if (isset($item['time'])) {
+            $item['time'] = date($format . $postfix, $item['time']);
+        }
+
+        unset($item['fdate']);
+        unset($item['ftime']);
+        unset($item['status']);
+
+        unset($item);
+    }
+
+    return $list;
+}
+
+
+function s_weibo_ago($time) {
+    if (s_bad_id($time)) {
+        $time = s_action_time();
+    }
+
+    $second = s_action_time() - $time;
+
+	if (( $diff = intval($second / (60 * 60 * 24) )) > 0 ) {
+        return $diff . "天前";
+
+    } else if (( $diff = intval($second / (60 *60) )) > 0) {
+        return $diff . "小时前";
+
+    } else if (( $diff = intval($second / 60) ) > 0) {
+        return $diff . "分钟前";
+	}
+
+	return "刚刚发表";
+}
+
+//返回以json格式的weibo数据，此处为做error_code检查
+function s_weibo_http($url, $params=false, $method="get") {
+    if (false === $params) {
+        $params = array();
+    }
+
+    //添加COOKIE
+    $params["cookie"]["SUE"] = $_COOKIE["SUE"];
+    $params["cookie"]["SUP"] = $_COOKIE["SUP"];
+
+    //添加APPKEY
+    $params["source"] = APP_KEY;
+
+    //上传图片。有两种情况
+    //  1、@/image/web.jpg
+    //  2、图片数据
+    //
+    if (isset($params["pic"])
+        && is_string($params["pic"])
+    ) {
+        //检查数据是二进制文件还是路径
+        $params["_name"] = "pic";
+
+        if ( substr($params["pic"], 0, 1) === '@' ) {
+            //@是路径
+            $params["_data"] = file_get_contents(substr($params["pic"], 1));
+
+        } else {
+            //直接使用
+            $params["_data"] = $params["pic"];
+        }
+
+
+        unset($params["pic"]);
+    }
+
+    //上传头像
+    if (isset($params["image"])
+        && is_string($params["image"])
+    ) {
+        //检查数据是二进制文件还是路径
+        $params["_name"] = "image";
+
+        if ( substr($params["image"], 0, 1) === '@' ) {
+            //@是路径
+            $params["_data"] = file_get_contents(substr($params["image"], 1));
+
+        } else {
+            //是图片数据
+            $params["_data"] = $params["image"];
+        }
+
+        unset($params["image"]);
+    }
+
+
+    if (false === ( $data = s_http_json($url, $params, $method) )
+        || isset($data['error_code'])
+    ) {
+        return s_err_action($data['error']);
+    }
+
+    return $data;
+}
+
+
+//返回用户前20条微博列表
+function s_weibo_list_by_uid($uid, $page=1, $count=20) {
+    if (s_bad_id($uid)
+        || s_bad_id($page)
+        || s_bad_id($count)
+
+        || $count > 200
+    ) {
+        return false;
+    }
+
+    //看cache中是否存在
+    $key = "weibo_list_by_uid#" . $uid . $page. $count;
+
+    if (false === ( $data = s_memcache($key) )) {
+        //缓存中没有，请求服务器
+        $params = array(
+            "user_id"   => $uid,
+            "count"     => $count,
+            "page"      => $page,
+        );
+
+        if (false === ( $data = s_weibo_http('http://api.t.sina.com.cn/statuses/user_timeline.json', $params) )) {
+            return false;
+        }
+       
+        //缓存起来900秒（15分钟）
+        //$mem->set($key, $data, 0, 900);
+    }
+
+    return $data;
+}
+
+
+//返回带gps信息的微博数据，默认每页20条
+function s_weibo_gps_list_by_uid($uid, $page=1, $count=20) {
+    if (s_bad_id($uid)
+        || s_bad_id($page)
+        || s_bad_id($count)
+
+        || $count > 200
+    ) {
+        return false;
+    }
+
+    //看cache中是否存在
+    $key = "weibo_gps_list_by_uid#" . $uid . $page. $count;
+
+    if (false === ( $data = s_memcache($key) )) {
+        //缓存中没有，请求服务器
+        $params = array(
+            "user_id"   => $uid,
+            "count"     => $count,
+            "page"      => $page,
+        );
+
+        if (false === ( $data = s_weibo_http('http://i2.api.weibo.com/2/place/user_timeline.json', $params) )) {
+            return false;
+        }
+
+        //缓存起来900秒（15分钟）
+        //$mem->set($key, $data, 0, 900);
+    }
+
+    return $data;
+}
+
+
+function s_weibo_2id_by_mids($mids, $type=1) {
+    $is_array = 0;
+
+    if (is_array($mids)) {
+        sort($mids, SORT_STRING);
+
+        $mids = implode(',', $mids);
+
+        $is_array = 1;
+    }
+
+
+    if (s_bad_string($mids)) {
+        return false;
+    }
+
+
+    //看cache中是否存在
+    $key = "weibo_2id_by_mids#" . $mids;
+
+    if (false === ( $data = s_memcache($key) )) {
+        //缓存中没有，请求服务器
+        $params = array(
+            "mid"       => $mids,
+            "type"      => $type,
+            "is_batch"  => $is_array,
+            "isBase62"  => 1,
+        );
+
+        if (false === ( $data = s_weibo_http('https://api.weibo.com/2/statuses/queryid.json', $params) )) {
+            return false;
+        }
+
+        //缓存起来24小时
+        s_memcache($key, $data, 24 * 3600);
+    }
+
+    if (!$is_array) {
+        //非数组查询，只返回对应的id
+        return $data['id'];
+    }
+
+
+    $ret = array();
+
+    //数组需要处理下
+    foreach ($data as &$item) {
+        $ret = array_merge($ret, $item);
+
+        unset($item);
+    }
+
+    unset($data);
+
+    return $ret;
+}
+
+
+//返回微博的详细信息
+//  $mid 可为简单数组 如 array('YQsdz13', 'YQsdz13', 'YQsdz13', 'YQsdz13', 'YQsdz13', 'YQsdz13')
+//       可为联合数组 如 array(array('mid' => 'YQsdz12'), array('mid' => 'YQsdz12'))
+//       可为mid字符  如 YQsdz13
+//
+function s_weibo_detail_by_mid($mid, $key=false) {
+    if (is_string($mid)) {
+        //查一个
+        $mid = array($mid);
+
+    } else if (is_array($mid)) {
+        //查多个
+        if (is_string($key)) {
+            //是一个联合数组，那么按$key取值
+            $list = $mid;
+
+            $mid = array();
+
+            foreach ($list as $item) {
+                if (!s_bad_string($item[$key], $id)) {
+                    $mid[] = $id;
+                }
+            }
+
+            unset($list);
+        }
+
+
+        $mid = array_unique($mid);
+    }
+
+    if (s_bad_array($mid)
+        //得到所有的id
+        || false == ( $mid = s_weibo_2id_by_mids($mid) )
+    ) {
+        return false;
+    }
+
+    //查询所有的微博详情
+    $list = array();
+
+    foreach ($mid as $key=>$wid) {
+        $list[$key] = s_weibo_by_wid($wid);
+    }
+
+
+    return $list;
+}
+
